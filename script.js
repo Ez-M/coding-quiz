bc2319737c1fa6f981d35404ee03f4fe683dae27
Ezequiel <martinez.ezequiel42@gmail.com>
2021-08-16 21:41:10 -0700

    LOTS of troubleshooting to have currentquestion.theAnswer update properly. Start butten added, timer text added, no functionality to timer yet. Page now begins with all buttons and text hidden until start button is pressed, which hides start button and reveals others.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var score = 0;
-var currentQuestion = "";
 var questionNum = 0;
+var currentQuestion = "";
+var timer = document.querySelector("#timer");
 var button1 = document.querySelector("#button1");
 var button2 = document.querySelector("#button2");
 var button3 = document.querySelector("#button3");
 var button4 = document.querySelector("#button4");
+var start = document.querySelector("#startButton");
 var h1El = document.querySelector("#prompt");
+var showAnswer = document.querySelector("p");
 
 /*question objects contain the promp and answers as well as correct answer for each question card*/
 var question1 = {
@@ -14,58 +17,61 @@ var question1 = {
     answer2: "2. whaterver",
     answer3: "3. whaterver",
     answer4: "4. whaterver",
-    theAnswer: 1
+    answer: "1",
 };
 
 var question2 = {
-    prompt: "the text for the question",
-    answer1: "1. whaterver",
+    prompt: "the text for the second question",
+    answer1: "1. pineapple",
     answer2: "2. whaterver",
     answer3: "3. whaterver",
     answer4: "4. whaterver",
-    theAnswer: 2
+    answer: "2",
 };
 
 var question3 = {
-    prompt: "the text for the question",
+    prompt: "the text for the third question",
     answer1: "1. whaterver",
     answer2: "2. whaterver",
     answer3: "3. whaterver",
     answer4: "4. whaterver",
-    theAnswer: 3
+    answer: "3",
 };
 
 var question4 = {
-    prompt: "the text for the question",
+    prompt: "the text for the fourth question",
     answer1: "1. whaterver",
     answer2: "2. whaterver",
     answer3: "3. whaterver",
     answer4: "4. whaterver",
-    theAnswer: 4
+    answer: "4",
 };
 
 var question5 = {
-    prompt: "the text for the question",
+    prompt: "the text for the fifth question",
     answer1: "1. whaterver",
     answer2: "2. whaterver",
     answer3: "3. whaterver",
     answer4: "4. whaterver",
-    theAnswer: 4
+    answer: "4",
 };
 
 var allQuestions = [question1, question2, question3, question4, question5];
-
 var currentQuestion = allQuestions[questionNum];
-
-console.log(currentQuestion.prompt);
+updatescreen();
 
 //needs update button function
+
+function updatescreen(){
+currentQuestion = allQuestions[questionNum];
+
 h1El.textContent = currentQuestion.prompt;
 button1.textContent = currentQuestion.answer1;
 button2.textContent = currentQuestion.answer2;
 button3.textContent = currentQuestion.answer3;
 button4.textContent = currentQuestion.answer4;
-
+return;
+} ;
 
 
 
@@ -73,26 +79,45 @@ button4.textContent = currentQuestion.answer4;
 
 
 //listens for a click anywhere on the page, and performs function
-document.addEventListener("click", function () {
+document.addEventListener("click", function (event) {
     //determines if click was on a button, if so, performs function
     if (event.target === button1 ||
         event.target === button2 ||
         event.target === button3 ||
         event.target === button4) {
-//compres the first character of the textContent of the click target with the "theAnswer"
+          //  console.log(event.target.textContent[0]);
+           // console.log(currentQuestion.answer);
+//compres the first character of the textContent of the click target with the "answer"
 //variable of the current quesstion
 //if correct, displays correct on screen, and increases questionNum by 1
-        if (event.target.textContent[0] == currentQuestion.theAnswer) {
+        if (event.target.textContent[0] === currentQuestion.answer) {
             console.log("Correct");
             questionNum++;
-            console.log(questionNum);
+            score++;
+            showAnswer.textContent = "Correct!"
+
+            updatescreen();
         }
         else {
-            console.log("wrong!");
+            console.log("Incorrect!");
+            showAnswer.textContent = "Incorrect!"
+            updatescreen();
         };
     }
     else {
         return;
     };
 
+});
+//All answer buttons, and timer start hidden. Revealed and timer started when start button pressed
+//start button is also hidden when clicked
+document.addEventListener("click",function(event) {
+    if (event.target === start) {
+        button1.setAttribute("style", "display: block;");
+        button2.setAttribute("style", "display: block;");
+        button3.setAttribute("style", "display: block;");
+        button4.setAttribute("style", "display: block;");
+        timer.setAttribute("style", "display:block");
+        start.setAttribute("style", "display: none;");
+    };
 });
